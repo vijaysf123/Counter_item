d97e83beb2f75f90aee1fbe05159445c0537ce53
Vijay Singh Fartyal <109033340+vijaysf123@users.noreply.github.com>
2022-10-06 17:45:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ counters.forEach(function(counter){
     }
     updateCounter();
 })
+
+
+// const countersEl = document.querySelectorAll(".counter");
+
+// countersEl.forEach((count) => {
+//   count.innerText = "0";
+//   incrementCounter();
+//   function incrementCounter() {
+//     let startCount = +count.innerText;
+//     const dataCeil = count.getAttribute("data-ceil");
+//     const increment = dataCeil / 15;
+//     startCount = Math.ceil(startCount + increment);
+
+//     if (startCount < dataCeil) {
+//       count.innerText = startCount;
+//       setTimeout(incrementCounter, 50);
+//     } else {
+//       count.innerText = dataCeil;
+//     }
+//   }
+// });
